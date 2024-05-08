59a24c3f00d0656ca598cfe455a1320771ba5b26
NwiZ <152017753+NirmataNetwork@users.noreply.github.com>
2024-05-08 10:32:50 +0300

    1.1.0

---
+++ src/common/ntp.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 Nirmata Network
+// Copyright (c) 2019 Zano Project
+
+#pragma once
+#include <cstdint>
+#include <string>
+
+namespace tools
+{
+
+  // requests current time via NTP from 'host_hame' using 'timeout_sec'
+  // may return zero -- means error
+  int64_t get_ntp_time(const std::string& host_name, size_t timeout_sec = 5);
+
+  // request time via predefined NTP servers
+  // may return zero -- mean error
+  int64_t get_ntp_time();
+
+} // namespace tools
